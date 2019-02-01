20e5ec080d46010618033789ad1e9e8080872821
robertfairley <robert@robertfairley.com>
2019-01-31 23:26:10 -0500

    [ Patch ] Mild changes to organization. Added CV page. Still need to add method to read post listings to be rendered. Should modify Page struct to allow for this, and add an object to hose 'post' related methods.

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
+run:
+	@go run src/main.go
+start:
+	./bin/server
 build:
 	make clean
 	go build -o bin/server src/main.go
 clean:
-	rm -rf ./bin
+	@rm -rf ./bin
 setup:
-	export GOPATH=$(pwd)
+	@export GOPATH=$(pwd)
 	go get -u gopkg.in/russross/blackfriday.v2
--- src/main.go
+++ src/main.go
@@ -16,11 +16,15 @@ import (
 
 // SiteSettings - settings JSON data struct
 type SiteSettings struct {
-	Title    string
-	Slogan   string
-	Notice   string
-	Hostname string
-	Port     string
+	Title     string
+	Slogan    string
+	Notice    string
+	Hostname  string
+	Port      string
+	PostExt   string
+	PostsDir  string
+	ViewsDir  string
+	StaticDir string
 }
 
 // Page - site page data
@@ -32,23 +36,16 @@ type Page struct {
 // StringKeyValue - JSON key:value as string
 type StringKeyValue map[string]string
 
-//
+// Globals
 var cwd, _ = os.Getwd()
-
-//
 var settings = loadSettings()
-
-//
-var staticPath = cwd + "/static/"
-
-//
-var viewsPath = cwd + "/views"
+var staticPath = cwd + settings.StaticDir
+var viewsPath = cwd + settings.ViewsDir
+var postsPath = cwd + settings.PostsDir
 
 // loadSettings - Load the site settins from a JSON to prevent
 // the need to recompile if certain global settings change.
 func loadSettings() SiteSettings {
-	cwd, _ := os.Getwd()
-
 	settingsPath := cwd + "/site-config.json"
 
 	settingsFileOutput, _ := ioutil.ReadFile(settingsPath)
@@ -57,11 +54,15 @@ func loadSettings() SiteSettings {
 	json.Unmarshal([]byte(settingsFileOutput), &result)
 
 	settingsOutput := SiteSettings{
-		Title:    result["title"],
-		Slogan:   result["slogan"],
-		Notice:   result["notice"],
-		Hostname: result["hostname"],
-		Port:     result["port"],
+		Title:     result["title"],
+		Slogan:    result["slogan"],
+		Notice:    result["notice"],
+		Hostname:  result["hostname"],
+		Port:      result["port"],
+		PostExt:   result["postExtension"],
+		PostsDir:  result["postsDir"],
+		ViewsDir:  result["viewsDir"],
+		StaticDir: result["staticDir"],
 	}
 
 	return settingsOutput
@@ -70,9 +71,7 @@ func loadSettings() SiteSettings {
 // getContents - Get the markdown contents of a post and
 // convert it to renderable HTML
 func getContents(path string) []byte {
-	cwd, _ := os.Getwd()
-
-	testFilePath := cwd + "/posts/" + path
+	testFilePath := postsPath + path
 	contents, err := ioutil.ReadFile(testFilePath)
 
 	if err != nil {
@@ -87,24 +86,45 @@ func getContents(path string) []byte {
 // getPostName - Parse the provided string and return
 // the full post filename.
 func getPostName(filepath string) string {
-	var extension string = ".md"
-
 	filename := path.Base(filepath)
-	postName := strings.Split(filename, extension)
+	postName := strings.Split(filename, settings.PostExt)
 
 	return postName[0]
 }
 
-//
+// getPostFilename - concatenate the standard blog post
+// file extension and the provided path to the post.
 func getPostFilename(postURL string) string {
-	var extension string = ".md"
-
-	return postURL + extension
+	return postURL + settings.PostExt
 }
 
-//
+// HomeRouteHandler - Response for the home page
+func HomeRouteHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles(viewsPath + "/home.html"))
+	data := Page{
+		Title:   settings.Title,
+		Content: "This is a test home page",
+	}
+	tmpl.Execute(w, data)
+}
+
+// AboutRouteHandler - Response for the about page
+func AboutRouteHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles(viewsPath + "/about.html"))
+	tmpl.Execute(w, nil)
+}
+
+// CvRouteHandler - Response for the CV page
+func CvRouteHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles(viewsPath + "/cv.html"))
+	tmpl.Execute(w, nil)
+}
+
+// PostRouteHandler - Response for any blog post. Parses the markdown file
+// and injects it into the page based on the `year/month/post-name`
+// path beneath `/posts/` sub-routes`
 func PostRouteHandler(w http.ResponseWriter, r *http.Request) {
-	postPath := strings.TrimPrefix(r.URL.Path, "/posts/")
+	postPath := strings.TrimPrefix(r.URL.Path, settings.PostsDir)
 
 	tmpl := template.Must(template.ParseFiles(viewsPath + "/post.html"))
 
@@ -118,25 +138,16 @@ func PostRouteHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles(viewsPath + "/home.html"))
-		data := Page{
-			Title:   settings.Title,
-			Content: "This is a test home page",
-		}
-		tmpl.Execute(w, data)
-	})
+	fmt.Print(settings)
 
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles(viewsPath + "/about.html"))
-		tmpl.Execute(w, nil)
-	})
-
-	http.HandleFunc("/posts/", PostRouteHandler)
+	http.HandleFunc("/", HomeRouteHandler)
+	http.HandleFunc("/about", AboutRouteHandler)
+	http.HandleFunc("/cv", CvRouteHandler)
+	http.HandleFunc(settings.PostsDir, PostRouteHandler)
 
 	fs := http.FileServer(http.Dir(staticPath))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle(settings.StaticDir, http.StripPrefix(settings.StaticDir, fs))
 
-	fmt.Printf("Listening at %s%s", settings.Hostname, settings.Port)
+	fmt.Printf("Listening at %s%s\n", settings.Hostname, settings.Port)
 	log.Fatal(http.ListenAndServe(settings.Port, nil))
 }
